dcdeb362f4afed93ba9a11406a311a5568f1a091
Pierre <pierre.cathelin@hubvisory.com>
2017-04-06 15:09:16 +0200

    HP update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
+// Menu class when scrolled
 $(function () {
   $(document).scroll(function () {
 	  var $nav = $(".navbar");
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
 });
+
+
