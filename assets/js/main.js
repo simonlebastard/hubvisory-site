e8a11295b03b950e6a7ee5b7ec8eec4d4d75a22e
Pierre <pierre.cathelin@hubvisory.com>
2017-04-06 19:58:04 +0200

    Home page work

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,18 @@ $(function () {
 	});
 });
 
-
+// Smooth scrolling
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 900);
+        return false;
+      }
+    }
+  });
+});
