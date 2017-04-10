d334a611a47c6344cd5a2b0767ddd73042b65dbc
Pierre <pierre.cathelin@hubvisory.com>
2017-04-10 19:40:01 +0200

    Work on Offer page

---
+++ assets/js/main2.js
@@ -0,0 +1,8 @@
+// Menu class when scrolled
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".navbar");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
+
